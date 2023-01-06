3d7ea49db32255e04c09d37b76b8fde71d5d81d5
kevincar <kevincarrolldavis@gmail.com>
2023-01-06 11:34:17 -0500

    Print channel Data

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,30 @@
 #undef UNICODE
 #include "AbmSdkInclude.h"
 
+void print_sample(float* sample, int n_channels) {
+    char time[256] = { 0 };
+    sprintf(time, "%d:%d:%d:%f", sample[2], sample[3], sample[4], sample[5]);
+    std::cout << "Epoch: " << sample[0] << "\t"
+        << "Offset: " << sample[1] << "\t"
+        << "Time: " << time << "\t";
+
+    /*for (int ch = 0; ch < n_channels; ch++) {
+        float value = sample[ch + 6];
+        std::cout << "ch" << ch << ": " << value << "\t";
+    }*/
+    std::cout << "POz: " << sample[16 + 6];
+    std::cout << std::endl;
+    return;
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+  if (!AttachConsole(-1)) {
+    MessageBox(NULL, L"Failed to attach console", L"ERROR", MB_ICONERROR | MB_OK);
+    return 0;
+  }
+  FILE* p;
+  freopen_s(&p, "CONOUT$", "w", stdout);
+  std::cout << "Attached!" << std::endl;
   _DEVICE_INFO* device_info = GetDeviceInfoKeepConnection(NULL);
   if (device_info == NULL) {
     MessageBox(NULL, L"Failed to find device", L"ERROR", MB_ICONERROR | MB_OK);
@@ -44,22 +67,20 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
   }
   MessageBox(NULL, L"Session started", L"INFO", MB_OK);
  
-  int n_samples = 5;
-  float* data = GetRawData(n_samples);
-  if (data == NULL) {
-    MessageBox(NULL, L"Failed to collect data", L"ERROR", MB_ICONERROR | MB_OK);
-    return 0;
+  while (true) {
+      int n_samples = 1;
+      float* data = GetRawData(n_samples);
+      if (data == NULL) {
+          continue;
+      }
+
+      print_sample(data, device_info->nNumberOfChannel);
   }
-  MessageBox(NULL, L"Data Collected!", L"INFO", MB_OK);
-  std::wstring data_message = L"";
-  for (int i = 0; i < 5; i++) {
-    data_message += std::to_wstring(data[i]) + L", ";
-  }
-  MessageBox(NULL, data_message.c_str(), L"Data", MB_OK); 
 
   StopAcquisitionKeepConnection();
   MessageBox(NULL, L"Hello, Message Box!", L"Message", MB_OK);
   
   CloseCurrentConnection();
+  std::cout << "Hello World" << std::endl;
   return 0;
 }
