96446ee74a140c85728ec3c61c46c9f8b8668fda
Kevin Davis <kevincarrolldavis@gmail.com>
2024-06-14 14:14:50 -0400

    Update triggering

--- abmbci/trigger.py
+++ abmbci/trigger.py
@@ -1,22 +1,22 @@
 from abmbciext import OutPacket
 from serial import Serial
-from serial.tools.list_ports import ListPortInfo, comports
+from serial.tools.list_ports import comports
+from serial.tools.list_ports_common import ListPortInfo
 from typing import Optional
 
 
 class Trigger:
-    def __init__():
-        self._pid: int = 0
+    def __init__(self):
         self._port: Optional[Serial] = None
         self._port_name: Optional[str] = None
-        self._vid: int = 0
+        self._hwid: str = "ACPI\\PNP0501\\1"
 
     @property
     def port(self) -> Serial:
         if self._port is not None:
             return self._port
 
-        self._port = Serial(self.port_name)
+        self._port = Serial(self.port_name, 57600)
         return self._port
 
     @property
@@ -27,15 +27,15 @@ class Trigger:
         potential_port: List[ListPortInfo] = [
             dev
             for dev in comports()
-            if (dev.pid == self._pid) and (dev.vid == self._vid)
+            if dev.hwid == self._hwid
         ]
 
         if len(potential_port) < 1:
             raise Exception("Failed to find trigger port")
 
-        self._port_name = potential_port[0].device_name
+        self._port_name = potential_port[0].device
         return self._port_name
 
-    def send(data: str) -> None:
+    def send(self, data: str) -> None:
         packet: OutPacket = OutPacket(data)
         self.port.write(packet.encode())
